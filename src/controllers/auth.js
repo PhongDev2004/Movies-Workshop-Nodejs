3014e7a7b5ca38452c9ad9324bb5b07c55071281
PhongLB_2004 <124508462+PhongDev2004@users.noreply.github.com>
2024-06-24 19:47:29 +0700

    U auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,27 +8,22 @@ import ApiError from "../utils/ApiError.js";
 class AuthController {
   async register(req, res, next) {
     try {
-      //B1: validate: email, password, username
       const { email, username, avatar, password } = req.body;
       const { error } = registerValidator.validate(req.body);
       if (error) {
         const errors = error.details.map((err) => err.message).join(", ");
         throw new ApiError(StatusCodes.BAD_REQUEST, errors);
       }
-      // b2: validate email exitsing
       const emailExist = await User.findOne({ email });
       if (emailExist)
         throw new ApiError(StatusCodes.BAD_REQUEST, "Email da duoc dang ky");
-      // b3 ma hoa password
       const hashPassword = await bcryptjs.hash(password, 10);
-      // update db
       const user = await User.create({
         email,
         username,
         avatar,
         password: hashPassword,
       });
-      // b4 remove password in res
       res.status(StatusCodes.OK).json({
         message: "Create Done",
         data: { ...user.toObject(), password: undefined },
@@ -42,18 +37,15 @@ class AuthController {
   async login(req, res, next) {
     try {
       const { email, password } = req.body;
-      //B1: validate: email, password
       const { error } = loginValidator.validate(req.body);
       if (error) {
         const errors = error.details.map((err) => err.message).join(", ");
         throw new ApiError(StatusCodes.BAD_REQUEST, errors);
       }
-      // check email xem co trong db
       const checkUser = await User.findOne({ email });
       if (!checkUser)
         throw new ApiError(StatusCodes.BAD_REQUEST, "Tai khoan ko hop le");
 
-      // so sanh password: bcryptjs
       const checkPassword = await bcryptjs.compare(
         password,
         checkUser.password
@@ -61,11 +53,9 @@ class AuthController {
       if (!checkPassword)
         throw new ApiError(StatusCodes.BAD_REQUEST, "Tai khoan ko hop le");
 
-      // ma hoa token
       const token = jwt.sign({ id: checkUser._id }, process.env.SECRET_KEY, {
         expiresIn: "1d",
       });
-      // res
       res.status(StatusCodes.OK).json({
         message: "Login ok",
         user: { ...checkUser.toObject(), password: undefined },
