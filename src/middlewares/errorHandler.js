6140c0cb450fb2b73ee737f7e8cb94859b192737
PhongLB_2004 <124508462+PhongDev2004@users.noreply.github.com>
2024-06-24 19:48:26 +0700

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,10 +4,6 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const errorMessage =
     err.message || StatusCodes[StatusCodes.INTERNAL_SERVER_ERROR];
-  // console.error(err.stack);
-  // notify slack
-  // send mail
-  // sentry
   res.status(statusCode).json({
     name: err.name,
     message: errorMessage,
